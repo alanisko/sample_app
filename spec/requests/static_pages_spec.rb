729d46559053b1b9c5140ac57abff9efca8d53c6
Alan Holsztynski <alanisko@gmail.com>
2013-11-27 14:01:43 -0500

    Add page title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "RoR Tutorial Sample App |"}
+  let(:base_title) { "RoR Tutorial Sample App"}
 
   describe "Home page" do
 
@@ -11,9 +11,13 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} Home")
+      expect(page).to have_title(base_title)
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("#{base_title} | Home")
     end
   end
 
@@ -26,7 +30,7 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -39,7 +43,7 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
@@ -52,7 +56,7 @@ describe "StaticPages" do
 
     it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} Contact Us")
+      expect(page).to have_title("#{base_title} | Contact Us")
     end
   end
 
